bee09c5f9e539b048476278289a8563d105485ff
yanks00 <101224468+yanks00@users.noreply.github.com>
2022-05-17 18:57:23 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,40 @@
+# inclui < stdio.h > 
+# inclui < stdlib.h > 
+
+void gerar_tabuada ( int ); 
+
+int main ( int argc, char *argv[]) { 
+   base int ; 
+
+  if (argc == 2 ) 
+    base = atoi (argv[ 1 ]); 
+  senão 
+    base = 2 ; 
+
+  gerar_tabuada (base); 
+  sair (EXIT_SUCCESS);
+} 
+
+void gerar_tabuada ( int base) { 
+   const char *arquivo = " tabela.tex " ; 
+
+   ARQUIVO * fp = fopen (arquivo, " wt " ); 
+   if (fp == NULL ) 
+       retorno ; 
+
+   fprintf (fp, " \\ begin{tabela}[h] \n " ); 
+   fprintf (fp, " \\ begin{tabular}{|rcrcr|} \n " ); 
+   
+   fprintf (fp, " \\ linha \n " ); 
+
+   for ( int i= 0 ; i<= 10 ; i++){ 
+       fprintf (fp, " %3d & $ \\ vezes$ & %3d & $=$ & %3d \\\\\n " , i, base, i*base); 
+   } 
+
+   fprintf (fp, " \\ linha \n " ); 
+   
+   fprintf (fp, " \\ end{tabular} \n " ); 
+   fprintf (fp, " \\ end{tabela} \n " ); 
+
+   fclose (fp);
+}
